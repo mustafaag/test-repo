a6100cb20a1c8032fa9f99912b249884e0419a0d
mustafaag <mustafaaga23@gmail.com>
2020-12-26 12:47:48 +0100

    gift card

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import data from './data/data.json'
+
+
 
 function App() {
+  console.log(data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+    <tr>
+    <th>Date</th>
+    <th>Total Amount</th>
+    </tr>
+     
+     {data.map(d => {
+       return(<tr key={d.id}>
+         <td>{d.Date}</td>
+       <td>{d.Price}</td>
+       </tr>)
+     })}
+      </table>
     </div>
   );
 }
